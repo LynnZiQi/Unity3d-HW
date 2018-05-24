9148e58d5dbf903b53e0c8d4355448f6d401337c
LynnZiQi <13719342006@163.com>
2018-05-24 22:50:23 +0800

    upload code

---
+++ HW7/ParticleSea.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSea : MonoBehaviour
+{
+
+    public ParticleSystem particleSystem;
+    private ParticleSystem.Particle[] particlesArray;
+    private myParticle[] particlesAtr;
+
+    public int seaResolution;
+    public static float MaxRadius = 100f;
+    public static float MinRadius = 50f;
+
+    public float radius = 100.0f;
+    public Gradient colorGradient;
+
+    void Start()
+    {
+        particlesArray = new ParticleSystem.Particle[seaResolution * seaResolution];
+        particlesAtr = new myParticle[seaResolution * seaResolution];
+        particleSystem.Emit(seaResolution * seaResolution);
+        particleSystem.GetParticles(particlesArray);
+        Init();
+
+    }
+    void Update()
+    {
+        ChangePosition();
+        ChangeColor();
+        particleSystem.SetParticles(particlesArray, particlesArray.Length);
+    }
+
+    void Init()
+    {
+        for (int i = 0; i < seaResolution; i++)
+        {
+            for (int j = 0; j < seaResolution; j++)
+            {
+                particlesAtr[i * seaResolution + j] = new myParticle(radius);
+                particlesArray[i * seaResolution + j].position = particlesAtr[i * seaResolution + j].GetPosition();
+            }
+        }
+        particleSystem.SetParticles(particlesArray, particlesArray.Length);
+    }
+
+    void ChangePosition()
+    {
+        for (int i = 0; i < seaResolution; i++)
+        {
+            for (int j = 0; j < seaResolution; j++)
+            {
+                particlesAtr[i * seaResolution + j].Rotate();
+                particlesArray[i * seaResolution + j].position = particlesAtr[i * seaResolution + j].GetPosition();
+            }
+        }
+    }
+    void ChangeColor()
+    {
+        float colorValue;
+        for (int i = 0; i < seaResolution; i++)
+        {
+            for (int j = 0; j < seaResolution; j++)
+            {
+                colorValue = (Time.realtimeSinceStartup - Mathf.Floor(Time.realtimeSinceStartup));
+                colorValue += particlesAtr[i * seaResolution + j].angle / 2 / Mathf.PI;
+                while (colorValue > 1)
+                    colorValue--;
+
+                //Debug.Log(colorValue);
+                particlesArray[i * seaResolution + j].startColor = colorGradient.Evaluate(colorValue);
+
+            }
+        }
+    }
+}
---
+++ HW7/myParticle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myParticle
+{
+
+    public float radius { get; set; }
+    public float speed { get; set; }
+    public float angle { get; set; }
+
+    public myParticle(float r)
+    {
+        this.radius = r;
+        this.speed = Random.value * Mathf.Sqrt(radius);
+        this.angle = Random.value * 2 * Mathf.PI;
+
+    }
+    public Vector3 GetPosition()
+    {
+        return radius * new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle));
+    }
+
+    public void Rotate()
+    {
+        this.angle += Time.deltaTime * speed / 10;
+        if (this.angle > 2 * Mathf.PI)
+            this.angle -= 2 * Mathf.PI;
+        this.radius += Random.value * 0.2f - 0.1f;
+        if (this.radius > ParticleSea.MaxRadius)
+            this.radius = ParticleSea.MaxRadius;
+        if (this.radius < ParticleSea.MinRadius)
+            this.radius = ParticleSea.MinRadius;
+    }
+}
