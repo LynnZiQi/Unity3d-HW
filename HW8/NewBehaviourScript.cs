c57688f319c065e4b207455444b8d1e7581ec114
LynnZiQi <13719342006@163.com>
2018-06-05 18:04:19 +0800

    upload code

---
+++ HW8/NewBehaviourScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+
+    private Transform m_Transform;
+
+    void Start()
+    {
+        m_Transform = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveControl();
+    }
+
+    void MoveControl()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            m_Transform.Translate(Vector3.forward * 0.1f, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            m_Transform.Translate(Vector3.back * 0.1f, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_Transform.Translate(Vector3.left * 0.1f, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            m_Transform.Translate(Vector3.right * 0.1f, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            m_Transform.Rotate(Vector3.up, -1.0f);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            m_Transform.Rotate(Vector3.up, 1.0f);
+        }
+
+    }
+}
